4eeaae2f8f9b739a65b72531ada4502005f32959
ckdudqls159 <54825908+ckdudqls159@users.noreply.github.com>
2019-10-01 11:04:56 +0900

    Add files via upload

---
+++ imgg.h
@@ -0,0 +1,32 @@
+﻿
+// imgg.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CimggApp:
+// 이 클래스의 구현에 대해서는 imgg.cpp을(를) 참조하세요.
+//
+
+class CimggApp : public CWinApp
+{
+public:
+	CimggApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CimggApp theApp;
